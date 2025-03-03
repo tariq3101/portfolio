1f9962a4f8612ce3ef080f743610ed32d8d522b8
tariq3101 <khantariq10648@gmail.com>
2025-03-03 09:46:36 +0530

    Changes for web analytics

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -3,6 +3,7 @@ import './Footer.css'
 import instagram from './images/instagram.png'
 import github from './images/github.png'
 import linkedin from './images/linkedin.png'
+import twitter from './images/twitter.png'
 
 const Footer = () => {
     return (
@@ -13,6 +14,7 @@ const Footer = () => {
                         <li><a href="https://www.instagram.com/_khantariq_/"><img src={instagram} alt='instagram'></img></a></li>
                         <li><a href="https://github.com/tariq3101/"><img src={github} alt='github'></img></a></li>
                         <li><a href="https://www.linkedin.com/in/tariq-khan-30088931a/"><img src={linkedin} alt='linkedin'></img></a></li>
+                        <li><a href='https://x.com/_khantariq_'><img src={twitter} alt='twitter'></img></a></li>
                     </ul>
                 </div>
                 <p>Tariq Khan &copy; 2025.</p>
